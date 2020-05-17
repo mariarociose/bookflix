655ecf4ade059c07e60382b9e2f83cd4f8785628
Mati <matiascicchittimg@gmail.com>
2020-05-17 20:40:50 -0300

    Se agrego la funcionalidad de borrar Novedad

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ var upload = multer();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
     next();
 });
 
--- server/routes/novedadesAdmin.js
+++ server/routes/novedadesAdmin.js
@@ -6,7 +6,7 @@ var protectedAdminRoute = require("../middlewares/protectedAdminRoute");
 
 router.get("/",protectedAdminRoute, function(req,res){
 
-    let query = "select * from novedades";
+    let query = "select * from novedades where habilitado = 1";
     connection.query(query,function(err,rows,fields){
 
 
@@ -36,6 +36,22 @@ router.post("/",function(req,res){
 
 })
 
+router.put("/",function(req,res){
+    console.log(req.body);
+    let query = `update novedades set habilitado = 0 WHERE id_novedad=${req.body.id}`;
+    connection.query(query,function(err,rows,fields){
+        if(err){
+            console.log(err);
+            res.status(500).send("Hubo un error");
+            return;
+        }
+        console.log(rows);
+        res.json(rows);
+
+    })
+
+
+})
 
 
 module.exports = router;

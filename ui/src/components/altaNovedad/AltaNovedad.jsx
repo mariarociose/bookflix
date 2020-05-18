9fde0d9be1c12cc4fc3adc59a9994a279963886f
Nicolas Prieto <malak32@gmail.com>
2020-05-18 16:41:26 -0300

    css alta novedad

--- ui/src/components/altaNovedad/AltaNovedad.jsx
+++ ui/src/components/altaNovedad/AltaNovedad.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-router-dom";
 import CommonDisplay from "../CommonDisplay";
-import "./altaNovedad.css"
+//import "./altaNovedad.css"
 
 class AltaNovedad extends CommonDisplay{
-    
+
     constructor(props){
         super(props);
     }
 
-    
+
     handleSubmit = (e) => {
         e.preventDefault();
         fetch("http://localhost:4000/novsAdmin",{
@@ -18,16 +18,27 @@ class AltaNovedad extends CommonDisplay{
         }).then(() => (this.props.history.push("/novedadesAdmin")))
         .catch((err) => (console.log(err)))
     }
-    
+
     renderContent(){
         return(
-        <div className="altaNovedadContainer">
-            <form action="" onSubmit={this.handleSubmit}>
+        <div className="book_form">
+            <form className="create_form" action="" onSubmit={this.handleSubmit}>
+              <fieldset className="create_field">
+
                 <label htmlFor="">Titulo:</label>
-                <input required type="text" name="titulo" id=""/>
+                <input required type="text" name="titulo" id="titulo"/>
+
                 <label htmlFor="">Descripción:</label>
-                <input required type="text" name="descripcion" id=""/>
-                <input type="submit" value="Agregar Novedad"/>
+                <input required type="text" name="descripcion" id="descripcion"/>
+                </fieldset>
+
+
+                <input type="submit" class="saveButton" value="Agregar Novedad"/>
+                <button type="reset" value="Cancelar" class="resetButton">
+                Cancelar
+                </button>
+
+
             </form>
 
         </div>)
